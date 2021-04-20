6101d8a40cf7c9a441bd74e545783f62c5e22f01
Siroshun09 <Siroshun09@users.noreply.github.com>
2021-04-20 02:51:44 +0000

    deploy: Siroshun09/Craftable@f9d89e152f6a252519e5dcc8aac99825484f0278

---
+++ _nuxt/static/1618887103/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002FAbout","\u002F","\u002Fjava\u002FLibrary","\u002Fjava\u002FMinecraft","\u002Fabout","\u002Fjava\u002Fminecraft","\u002Fjava\u002Flibrary"]})
